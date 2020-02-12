dfa3e8d51734f6dde012b3fedfd60241b16af22a
Tomasz Siwiec <tomasz.siwiec@motorolasolutions.com>
2020-02-12 15:30:37 +0100

    [ZEDAPP-4]Added Status enum

---
+++ src/main/java/com/zedapp/domain/Status.java
@@ -0,0 +1,13 @@
+package com.zedapp.domain;
+
+public enum Status {
+    ENTERED,
+    IN_DEVELOPMENT,
+    IN_PRODUCTION,
+    PRODUCED,
+    FOR_PICKUP,
+    SENT,
+    DELIVERED,
+    CANCELLED,
+    DONE;
+}
