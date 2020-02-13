3eaa47257228ec04208c95d840c7667c49176eea
Tomasz Siwiec <tomasz.siwiec@motorolasolutions.com>
2020-02-13 15:12:41 +0100

    [ZEDAPP-7]Added Order repository

---
+++ src/main/java/com/zedapp/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.zedapp.repository;
+
+import com.zedapp.domain.Order;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends GenericRepository<Order> {
+}
