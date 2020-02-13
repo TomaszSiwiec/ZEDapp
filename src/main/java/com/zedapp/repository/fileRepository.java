78190544ee42dc16170d04e74886dbb9171a03e8
Tomasz Siwiec <tomasz.siwiec@motorolasolutions.com>
2020-02-13 15:19:39 +0100

    [ZEDAPP-11]Added File repository

---
+++ src/main/java/com/zedapp/repository/fileRepository.java
@@ -0,0 +1,8 @@
+package com.zedapp.repository;
+
+import com.zedapp.domain.File;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface fileRepository extends GenericRepository<File> {
+}
