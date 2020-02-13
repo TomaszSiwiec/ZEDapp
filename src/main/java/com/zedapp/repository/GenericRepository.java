d9d9b6a8f5add7279c9ffba176a014a8bec54638
Tomasz Siwiec <tomasz.siwiec@motorolasolutions.com>
2020-02-13 15:07:33 +0100

    [ZEDAPP-6]Added generic repository

---
+++ src/main/java/com/zedapp/repository/GenericRepository.java
@@ -0,0 +1,26 @@
+package com.zedapp.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+@NoRepositoryBean
+public interface GenericRepository<T> extends CrudRepository<T, Integer> {
+    @Override
+    List<T> findAll();
+
+    Optional<T> findById(long id);
+
+    @Override
+    <S extends T> S save(S entity);
+
+    void delete(T entity);
+
+    default T findOrThrow(long id) throws EntityNotFoundException {
+        T object = findById(id).orElseThrow(() -> new EntityNotFoundException("Entity with id: " + " was not found!"));
+        return object;
+    }
+}
