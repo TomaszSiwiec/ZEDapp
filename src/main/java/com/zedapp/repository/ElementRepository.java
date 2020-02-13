978b222bbbe3021ae79ec5aa424482f90aa25938
Tomasz Siwiec <tomasz.siwiec@motorolasolutions.com>
2020-02-13 15:18:30 +0100

    [ZEDAPP-10]Added Element repository

---
+++ src/main/java/com/zedapp/repository/ElementRepository.java
@@ -0,0 +1,8 @@
+package com.zedapp.repository;
+
+import com.zedapp.domain.Element;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ElementRepository extends GenericRepository<Element> {
+}
