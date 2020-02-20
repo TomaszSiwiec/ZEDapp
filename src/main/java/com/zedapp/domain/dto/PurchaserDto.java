3554d46d104baabd0cae383a77d418994c90f3f7
Tomasz Siwiec <tomasz.siwiec@motorolasolutions.com>
2020-02-20 17:00:42 +0100

    [ZEDAPP-13]Added Purchaser DTO class

---
+++ src/main/java/com/zedapp/domain/dto/PurchaserDto.java
@@ -0,0 +1,15 @@
+package com.zedapp.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PurchaserDto {
+    private String name;
+    private String lastname;
+}
