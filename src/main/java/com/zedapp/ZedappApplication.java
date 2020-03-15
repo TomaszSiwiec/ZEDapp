9c38457f0dee197f04d3f3b6f7b8d62a68e2119e
Tomasz Siwiec <tomasz.siwiec@motorolasolutions.com>
2020-03-15 22:26:38 +0100

    [ZEDAPP-69] Added front-end part with framework Vaadin

--- src/main/java/com/zedapp/ZedappApplication.java
+++ src/main/java/com/zedapp/ZedappApplication.java
@@ -2,9 +2,10 @@ package com.zedapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ZedappApplication {
 
     public static void main(String[] args) {
